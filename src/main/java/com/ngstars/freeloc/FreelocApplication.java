b1b2bf0774b00fee20c943bf6bafec1d0cc715c0
teloumaz <manzamatelou@gmail.com>
2020-07-15 13:06:22 +0000

    Initialisation du projet

---
+++ src/main/java/com/ngstars/freeloc/FreelocApplication.java
@@ -0,0 +1,13 @@
+package com.ngstars.freeloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngstars/freeloc/FreelocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngstars.freeloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
